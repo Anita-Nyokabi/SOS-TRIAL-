81b21ccfc3f05ac757c199c7dc18ab74f64b70d4
Anita-Nyokabi <160007807+Anita-Nyokabi@users.noreply.github.com>
2024-06-15 01:30:14 +0300

    report_model.dart

---
+++ report_model.dart
@@ -0,0 +1,8 @@
+class ReportModel {
+  String description;
+  String location;
+  Uint8List evidence;
+  bool anonymous;
+
+  ReportModel({this.description, this.location, this.evidence, this.anonymous});
+}
